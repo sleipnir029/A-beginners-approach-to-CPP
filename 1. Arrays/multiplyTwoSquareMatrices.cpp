b31ef52034ebe31ede3614dca67ed724ab50a310
Rakibuzzaman Rahat <52344836+sleipnir029@users.noreply.github.com>
2020-06-15 00:01:09 +0600

    multiplyTwoSquareMatrices

---
+++ 1. Arrays/multiplyTwoSquareMatrices.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int m1[][2] = {{1, 2}, {3, 4}};
+    int m2[][2] = {{1, 1}, {1, 1}};
+    int res[2][2];
+    int i, j, k;
+
+    for (i=0; i<2; i++){
+        for (j=0; j<2; j++){
+            res[i][j] = 0;
+            for (k=0; k<2; k++){
+                res[i][j] += m1[i][k] * m2[k][j];
+            }
+        }
+    }
+
+    for (i=0; i<2; i++){
+        for (j=0; j<2; j++){
+            cout << res[i][j] << " ";
+            cout << "\n";
+        }
+    }
+
+    return 0;
+}
+
