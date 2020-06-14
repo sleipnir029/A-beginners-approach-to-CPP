f30881f5ad45665827c8d4daa50257c4a559473a
Rakibuzzaman Rahat <52344836+sleipnir029@users.noreply.github.com>
2020-06-15 00:09:00 +0600

    added link

--- 1. Arrays/largestElement.cpp
+++ 1. Arrays/largestElement.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(){
     int arr[] = {10, 20, 105, 100};
     int m = arr[0];
-    int n = sizeof(arr) / sizeof(arr[0]);
+    int n = sizeof(arr) / sizeof(arr[0]);              // https://stackoverflow.com/a/33523811
     for (int i=0; i<n; i++){
         if (m < arr[i])
             m = arr[i];
