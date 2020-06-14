5392c9816fe00a918e5ec14049df13766b5d59c3
Rakibuzzaman Rahat <52344836+sleipnir029@users.noreply.github.com>
2020-06-14 20:19:54 +0600

    added function

--- 1. Arrays/sumOfElementsInAnArray.cpp
+++ 1. Arrays/sumOfElementsInAnArray.cpp
@@ -2,13 +2,19 @@
 
 using namespace std;
 
+int sum(int arr[], int n);
+
 int main(){
     int arr[] = {1, 2, 3};
     int n = sizeof(arr)/ sizeof(arr[0]);
+    sum(arr, n);
+    return 0;
+}
+
+int sum(int arr[], int n){
     int sum = 0;
     for (int i=0; i<n; i++){
         sum += arr[i];
     }
     cout << sum;
-    return 0;
 }
